2219129dc05038d750b1718ab38cd93b45f36486
devkhchua <developer.khchua@gmail.com>
2021-11-08 21:47:42 +0800

    microservice api gateway

---
+++ src/main/java/com/ms/resources/api/gateway/FallbackController.java
@@ -0,0 +1,18 @@
+package com.ms.resources.api.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/authfallback")
+    public String userServiceFallbackMethod(){
+        return "User service is taking longer than expected. " + "Please try again later.";
+    }
+
+    @GetMapping("/quotefallback")
+    public String quoteServiceFallbackMethod(){
+        return "Quotation service is taking longer than expected. " + "Please try again later.";
+    }
+}
